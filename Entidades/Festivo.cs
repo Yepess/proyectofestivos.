d0273deaaed3bc84cea0eff392760a7a448ac782
Valery <valerymanco93@gmail.com>
2025-02-26 13:11:37 -0500

    Primer commit - Subiendo el proyecto

---
+++ Entidades/Festivo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFestivos.Dominio.Entidades
+{
+    
+        [Table("Festivo")]
+        public class Festivo
+        {
+            [Column("Id")]
+            public int Id { get; set; }
+
+            [Column("Nombre"), StringLength(100)]
+            public required string Nombre { get; set; }
+
+            [Column("Dia")]
+            public int Dia { get; set; }
+
+            [Column("Mes")]
+            public int Mes { get; set; }
+
+            [Column("DiasPascua")]
+            public int DiasPascua { get; set; }
+
+            [Column("IdTipo")]
+            public int IdTipo { get; set; }
+
+            public Tipo Tipo { get; set; }
+        }
+}
---
+++ Entidades/Tipo.cs
@@ -0,0 +1,19 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace ProyectoFestivos.Dominio.Entidades
+{
+    [Table("Tipo")]
+    public class Tipo
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Tipo"), StringLength(100)]
+        public required string Nombre { get; set; }
+
+        public ICollection<Festivo> Festivos { get; set; }
+    }
+}
